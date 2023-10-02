4561826a5648361f89096cf4e076c02b96a2c224
suryasaladi <nagusaladi20@gmail.com>
2023-10-02 13:55:18 +0530

    changes on table assertions

--- cypress/e2e/task.cy.js
+++ cypress/e2e/task.cy.js
@@ -1,16 +1,26 @@
-
 describe('test', () => {
   it('opening url', () => {
     cy.visit('https://testpages.herokuapp.com/styled/tag/dynamic-table.html')
     cy.get('summary').click()
-     cy.fixture('example.json').as('jsonData')
+     
     cy.fixture('example.json').then((data)=>{
       const  jsonString = JSON.stringify(data)
       cy.get('textarea').clear()
       .type(jsonString, { parseSpecialCharSequences: false })
     })
     cy.get('button').click()
-    cy.get('table').should('contain','Jennifer')
+    cy.fixture('example.json').then((tableData)=>{
+      const size = tableData.length;
+      cy.get("table tr").as("tableRows");
+
+      cy.get("@tableRows").should('have.length', size+1);
+      tableData.forEach((rowData, index) => {
+        cy.get("@tableRows").eq(index + 1).find("td").eq(0).should("contain.text", rowData.name);
+        cy.get("@tableRows").eq(index + 1).find("td").eq(1).should("contain.text", rowData.age);
+        cy.get("@tableRows").eq(index + 1).find("td").eq(2).should("contain.text", rowData.gender);
+      });
+      
+    })
   })
 })
 
